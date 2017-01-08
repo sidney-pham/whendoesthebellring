6de8ba835a5bc77f4868388f911089b9cb4bfa4a
Sidney Pham <sidneypham1@gmail.com>
2017-01-08 23:06:03 +1100

    No cool stuff here yet.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('test').innerHTML = 'Dynamic content!!!';
+
+    document.getElementById('checkToken').addEventListener('click', function() {
+        var token;
+
+        var request = new XMLHttpRequest();
+        request.open('GET', '/token');
+        request.addEventListener('load', function() {
+            if (request.status == 200) {
+                token = JSON.parse(this.responseText);
+            } else {
+                token = '/token fucked up: ' + this.response || 'No error message.';
+            }
+            document.getElementById('token').innerHTML = JSON.stringify(token, null, 2);
+        });
+        request.send(null);
+    });
 });
