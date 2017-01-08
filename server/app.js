8948e1c96d887897992932c759efed98b3c3b0e3
Sidney Pham <sidneypham1@gmail.com>
2017-01-08 22:46:00 +1100

    Finished the awesome server based off Classes. Way cooler. Got stuck because I didn't realise that you can't use LocalStorage on server side.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var path = require('path');
-var compression = require('compression');
+var express = require('express'),
+    session = require('express-session'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    compression = require('compression'),
+    auth = require('./auth'),
+    app = express();
 
 const PORT = parseInt(process.argv[2], 10) || 3000;
 
-var app = express();
-
 // Compress response bodies for all requests.
 app.use(compression());
+
+// Enable reading of cookies.
+app.use(cookieParser());
+
+// Get config file and call auth function with appropriate arguments.
+var config = require('./config').config;
+auth(app, config.clientId, config.clientSecret, config.host + ":" + PORT);
 
 // Serve static files at ../public to /.
 app.use(express.static(path.join(__dirname, '../public')));
